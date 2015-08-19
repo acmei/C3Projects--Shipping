36708056c7bc3a6b02ea6b2f0ca9129f1fc4630f
Jeri <drvonnjerryxlii@gmail.com>
2015-08-19 13:44:36 -0700

    missed a piece in the VCR setup

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -4,4 +4,5 @@ require "vcr"
 VCR.configure do |config|
   config.cassette_library_dir = "spec/vcr"
   config.hook_into :webmock # FALSIFY THE INTERNETS. MOCK THEM.
+  # config.default_cassette_options = { :record => :once }
 end
