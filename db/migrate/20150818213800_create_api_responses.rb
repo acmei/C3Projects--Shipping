a2e45b6503147bb6ca03f2604ba5034482112667
Jeri <drvonnjerryxlii@gmail.com>
2015-08-20 21:41:46 -0700

    attempted fix 2 for heroku db:migrate issue

--- db/migrate/20150818213800_create_api_responses.rb
+++ db/migrate/20150818213800_create_api_responses.rb
@@ -1,7 +1,7 @@
 class CreateApiResponses < ActiveRecord::Migration
   def change
-    create_table :api_responses do |t|
-
+    create_table :api_responses do |t| # this is a foreign_key to a betsy object
+      t.integer :order_id, foreign_key: false
       t.timestamps null: false
     end
   end
